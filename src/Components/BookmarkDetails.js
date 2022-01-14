0143f86f1beb0591365abd09de284ed630f0caad
Colin Jaffe <balloonasaurus@gmail.com>
2022-01-14 13:32:53 -0500

    Initial commit.

---
+++ src/Components/BookmarkDetails.js
@@ -0,0 +1,57 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+import { Link, useParams } from "react-router-dom";
+
+function BookmarkDetails() {
+  const URL = process.env.REACT_APP_API_URL;
+  const [bookmark, setBookmark] = useState([]);
+  const { index } = useParams();
+
+  // - make GET request to http://localhost:3003/bookmarks/:index
+  // - use `setBookmark` to change our current bookmark
+  //   to the data we get back
+  useEffect(() => {
+    axios
+      .get(`${URL}/bookmarks/${index}`)
+      .then((response) => setBookmark(response.data));
+  }, []);
+
+  const handleDelete = () => {};
+
+  return (
+    <article>
+      <h3>
+        {bookmark.isFavorite ? <span>⭐️</span> : null} {bookmark.name}
+      </h3>
+      <h5>
+        <span>
+          <a href={bookmark.url}>{bookmark.name}</a>
+        </span>{" "}
+        &nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
+        {bookmark.url}
+      </h5>
+      <h6>{bookmark.category}</h6>
+      <p>{bookmark.description}</p>
+      <div className="showNavigation">
+        <div>
+          {" "}
+          <Link to={`/bookmarks`}>
+            <button>Back</button>
+          </Link>
+        </div>
+        <div>
+          {" "}
+          <Link to={`/bookmarks/${index}/edit`}>
+            <button>Edit</button>
+          </Link>
+        </div>
+        <div>
+          {" "}
+          <button onClick={handleDelete}>Delete</button>
+        </div>
+      </div>
+    </article>
+  );
+}
+
+export default BookmarkDetails;
